523177c2afe245c4cd0f2ed7e1d61f56268898ac
Lixmna <kang7145471@naver.com>
2021-03-31 21:39:45 +0900

    스킬트리

---
+++ 스킬트리.py
@@ -0,0 +1,27 @@
+def solution(skill, skill_trees):
+    answer = 0
+    skill = list(map(str, skill))
+    skill_check = 0
+    skill_lenth = len(skill)
+
+    answer_check = True
+
+    for i in skill_trees :
+
+        temp = list(map(str, i))
+        skill_check = 0
+        answer_check = True
+        for j in range(0,len(temp)) : 
+            if temp[j] in skill :
+                if temp[j] == skill[skill_check] :
+                    skill_check = skill_check + 1
+                else :
+                    answer_check = False
+                    break
+
+        if answer_check == True :
+            answer += 1
+
+
+    return answer
+    
