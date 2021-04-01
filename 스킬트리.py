080376ae3122dd64889a037d9740994d78871402
Lixmna <kang7145471@naver.com>
2021-04-01 23:51:55 +0900

    스킬트리 프린터 오류수정

--- 스킬트리.py
+++ 스킬트리.py
@@ -2,7 +2,7 @@ def solution(skill, skill_trees):
     answer = 0
     skill = list(map(str, skill))
     skill_check = 0
-    
+
     answer_check = True
 
     for i in skill_trees :
@@ -10,6 +10,7 @@ def solution(skill, skill_trees):
         temp = list(map(str, i))
         skill_check = 0
         answer_check = True
+        
         for j in range(0,len(temp)) : 
             if temp[j] in skill :
                 if temp[j] == skill[skill_check] :
